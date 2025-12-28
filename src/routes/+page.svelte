d90264b935a6f8fc1b292806abe1cc68f4566c9f
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-12-28 19:50:27 -0300

    feat: show users

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { getUsers, loadUsers } from "$lib/state/user.svelte";
+    import UserGrid from "$lib/components/UserGrid.svelte";
+
+    $effect(() => {
+        loadUsers()
+    })
+
+    const users = $derived(getUsers());
+
+</script>
+
+<h1>Users</h1>
+
+<UserGrid {users}/>
