7ef2ccba1f3f7b79d5732620080f528de7ecbac8
jpsalles21 <joaopedrosalles.dev@gmail.com>
2025-12-28 19:49:23 -0300

    chore: add map http errors

---
+++ src/lib/api/httpError.ts
@@ -0,0 +1,17 @@
+import type { ApiError } from '$lib/types/api-error'
+
+export function mapHttpError(status: number): ApiError {
+  switch (status) {
+    case 401:
+      return { type: 'unauthorized' }
+
+    case 404:
+      return { type: 'not_found' }
+
+    case 500:
+      return { type: 'unknown' }
+
+    default:
+      return { type: 'unknown' }
+  }
+}
